1190e8656451bfa0c26a0de2ed473e73e257e7b7
marcoandreazampini <42516577+marcoandreazampini@users.noreply.github.com>
2021-06-08 12:20:24 +0200

    copied math_functions

---
+++ math_functions.py
@@ -0,0 +1,18 @@
+import pytest
+
+def sum(a,b):
+  return a + b
+
+def test_sum
+  assert sum(2,4) == 6 
+
+  
+  
+def banafshedivide(a,b):
+  return a / b
+
+def multiMarco(a,b):
+  return a**b
+
+if __name__ == '__main__':
+  a = sum(banafshedivide(multiMarco(3,4),6),10))
